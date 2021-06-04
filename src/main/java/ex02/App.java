4defbfc98b4a596b387652cbc1d7b39e39f3a22e
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-04 18:37:34 -0400

    Completed code.

--- src/main/java/ex02/App.java
+++ src/main/java/ex02/App.java
@@ -1,4 +1,12 @@
 package ex02;
+import java.util.Scanner;
 
 public class App {
+    public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String name = in.nextLine();
+
+        System.out.println(name + " has " + name.length() + " characters.");
+    }
 }
